391d8e05dfd2eecddf3b4cd065f846afae12edb2
shreegagan <104724964+shreegagan@users.noreply.github.com>
2024-04-19 16:21:38 +0530

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,7 +1,7 @@
-# resource "azurerm_resource_group" "rg" {
-#   name     = var.resource_group_name
-#   location = var.resource_group_location
-# }
+ resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+ location = var.resource_group_location
+}
 
 
 # Define the Azure virtual network
