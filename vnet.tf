5e4d85d426974afa0b3a767e03d461b2e5793b85
shreegagan <104724964+shreegagan@users.noreply.github.com>
2024-04-19 16:26:28 +0530

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,22 +1,21 @@
- resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
- location = var.resource_group_location
-}
+# resource "azurerm_resource_group" "rg" {
+ # name     = var.resource_group_name
+# location = var.resource_group_location
+#}
 
 
 # Define the Azure virtual network
 resource "azurerm_virtual_network" "vnet" {
   name                = var.virtual_network_name
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
 }
-
 
 # Define the Azure subnet within the virtual network
 resource "azurerm_subnet" "snet" {
   name                 = var.subnet_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = var.virtual_network_name
+  resource_group_name  = var.resource_group_name
   address_prefixes     = ["10.0.1.0/24"]
 }
