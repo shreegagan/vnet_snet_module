204eccae450c75cba83b61d29ef2b35f214f8cc2
shreegagan <104724964+shreegagan@users.noreply.github.com>
2024-05-27 10:19:05 +0530

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -18,4 +18,8 @@ resource "azurerm_subnet" "snet" {
   virtual_network_name = var.virtual_network_name
   resource_group_name  = var.resource_group_name
   address_prefixes     = ["10.0.1.0/24"]
+
+ depends_on = [
+    azurerm_virtual_network.vnet
+  ]
 }
