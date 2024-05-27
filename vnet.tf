5f427892f1ab6ed3f2a341aa696eefdf3228243f
shreegagan <104724964+shreegagan@users.noreply.github.com>
2024-05-27 10:14:40 +0530

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -8,7 +8,7 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = var.virtual_network_name
   address_space       = ["10.0.0.0/16"]
-  location            = var.location
+  location            = var.resource_group_location
   resource_group_name = var.resource_group_name
 }
 
